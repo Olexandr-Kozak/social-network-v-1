bbf2bfe5a2a4a87b0c788ba53e866c6ea95aab55
Olexandr-Kozak <123456sachko5@gmail.com>
2022-05-05 16:11:01 +0300

    less 49

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from "react";
 import Header from './components/Header/Header';
-import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
@@ -9,6 +8,7 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavBarContainer from "./components/NavBar/NavBarContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
     return (
@@ -23,6 +23,7 @@ const App = (props) => {
                        render={() => <Profile/>}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
+                <Route path='/users' render={() => <UsersContainer/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
             </div>
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,6 +17,7 @@ const NavBar = (props) => {
           <li className={s.item}><NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink></li>
           <li className={s.item}><NavLink to="/dialogs" activeClassName={s.activeLink}>Message</NavLink></li>
           <li className={s.item}><NavLink to="/news" activeClassName={s.activeLink}>News</NavLink></li>
+          <li className={s.item}><NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink></li>
           <li className={s.item}><NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink></li>
           <li className={s.item}><NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
           </li>
