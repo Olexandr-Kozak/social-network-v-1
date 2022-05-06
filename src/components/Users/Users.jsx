d9bcb3d5e6d9d57eaf5d506b461343f1fc6ca68e
Olexandr-Kozak <123456sachko5@gmail.com>
2022-05-06 15:20:22 +0300

    less 52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,18 +5,18 @@ import userPhoto from '../../Assets/images/logo-photo.jpg'
 
 const Users = (props) => {
 
-  if (props.users.length === 0) {
-
-
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-      props.setUsers(response.data.items)
-    });
-
-
+  let getUsers =()=>{
+    if (props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items)
+      });
+    }
   }
 
+
   return (
-    <div>Users
+    <div>
+      <button onClick={getUsers}>Get Users</button>
 
       {props.users.map(u => <div key={u.id}>
         <span>
